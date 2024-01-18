3821aff94a70a9edfbf0f5ca5f98f1a24da46f18
KhChanon <chanon.619@hotmail.com>
2024-01-18 15:35:05 +0700

    Finish Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
+
+// page
+import Homepage from './pages/Homepage/Homepage';
+import Patientinfo from './pages/Patientinfo/Patientinfo';
+import Followup from './pages/Followup/Followup';
+import FAQ from './pages/FAQ/FAQ';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/> 
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/patientinfo" element={<Patientinfo />} />
+        <Route path="/followup" element={<Followup />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </Router>
   );  
 }
 
