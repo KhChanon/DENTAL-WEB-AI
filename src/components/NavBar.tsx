21bb454660b6ae4c50ae502115b2e2ee44d20cf0
ninetynine <nineza321@gmail.com>
2024-01-19 13:21:32 +0700

    new nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="absolute top-[0rem] left-[0rem] bg-darkslateblue-200 overflow-hidden flex flex-row items-start justify-start py-[1.25rem] px-[1.88rem]">
+        <div className="flex flex-row items-center justify-start gap-[76.63rem]">
+          <img
+            className="relative w-[5.55rem] h-[3.13rem] object-cover"
+            alt=""
+            src="/output-image1@2x.png"
+          />
+          <div className="w-[4.06rem] flex flex-row items-center justify-between">
+            <div className="relative font-semibold">Login</div>
+          </div>
+        </div>
+      </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gainsboro: {
-          "100": "#d9d9d9",
-          "200": "rgba(230, 230, 230, 0.2)",
-        },
-        gray: "rgba(255, 255, 255, 0.2)",
-        limegreen: {
-          "100": "#57da36",
-          "200": "#24a803",
-        },
-        "colors-white-white": "#fff",
         darkslateblue: {
           "100": "#8660a9",
           "200": "#21294c",
         },
-        plum: "#a17ac2",
+        "colors-white-white": "#fff",
+        limegreen: "#57da36",
+        gray: "rgba(255, 255, 255, 0.2)",
       },
       spacing: {},
       fontFamily: {
@@ -29,24 +21,12 @@ module.exports = {
       },
     },
     fontSize: {
-      "5xl": "24px",
-      lgi: "19px",
-      "13xl": "32px",
-      "7xl": "26px",
-      "45xl": "64px",
-      "19xl": "38px",
-      "32xl": "51px",
+      "5xl": "1.5rem",
       inherit: "inherit",
     },
     screens: {
-      l: {
-        raw: "screen and (max-width: 950px)",
-      },
-      m: {
-        raw: "screen and (max-width: 700px)",
-      },
-      s: {
-        raw: "screen and (max-width: 450px)",
+      md: {
+        max: "960px",
       },
     },
   },
