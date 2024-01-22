58d18d60e0f14a547ccf40c83f14552b0f419867
KhChanon <chanon.619@hotmail.com>
2024-01-22 15:12:15 +0700

    Add home page link to logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,7 @@ const NavBar = () => {
         className="flex h-full ml-5"
         alt=""
         src={Logo}
+        onClick={() => {window.location.href = '/'}}
       />
       <div className='flex flex-row bg-darkslateblue-200 items-center justify-between h-fit mr-7 text-white border-0 text-2xl font-bold cursor-pointer select-none'>
         Login
