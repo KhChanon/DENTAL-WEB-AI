ce1924a64cea122f30f6ffd820c0944a4e8a81b0
KhChanon <chanon.619@hotmail.com>
2024-01-22 17:54:37 +0700

    Add MongoDB connection to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
+require('dotenv').config({ path: '../.env' })
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 
 const app = express();
+
+try {
+    mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected`);
+} catch (err) {
+    console.error(err);
+    process.exit(1);
+}
 
 app.use(cors());
 app.use(express.json());
