f151276637fc07f9d60cbe749fc91407d6996327
KhChanon <chanon.619@hotmail.com>
2024-04-26 18:19:33 +0700

    Add dialogflow-fulfillment library to server/package.json and update dialogflow.routes.js to use PostRecommendation instead of recommendation

--- server/routes/dialogflow.routes.js
+++ server/routes/dialogflow.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { recommendation } = require('../controller/dialogflow.controller');
+const { PostRecommendation } = require('../controller/dialogflow.controller');
 
 // POST a dialogflow response
 // Endpoint: /api/dialogflow/
-router.route('/').post(recommendation);
+router.route('/').post(PostRecommendation);
 
 module.exports = router;
