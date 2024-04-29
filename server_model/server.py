a5cfad4980c33a332e8dac5a8b1bd5d0a2bad5b4
KhChanon <chanon.619@hotmail.com>
2024-04-29 17:39:02 +0700

    Refactor server.py to use environment variables for MongoDB URI and Channel Access Token

--- server_model/server.py
+++ server_model/server.py
@@ -9,7 +9,7 @@ from flask_pymongo import PyMongo
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = dotenv.get_key('MONGO_URI')
+app.config["MONGO_URI"] = dotenv.get_key(dotenv.find_dotenv(), 'MONGO_URI')
 cors = CORS(app)
 mongo = PyMongo(app)
 
@@ -110,7 +110,7 @@ def send_followup():
             
             if len(contents) > 0:
                 requests.post('https://api.line.me/v2/bot/message/push',
-                        headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key("CHANNEL_ACCESS_TOKEN")}'},
+                        headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key(dotenv.find_dotenv(),"CHANNEL_ACCESS_TOKEN")}'},
                             json = {"to":  user['lineopenid'],
                                     "messages": [{
                                         "type": "flex",
@@ -202,7 +202,7 @@ def handle_message(body_json):
     
     if operation == -1:
         requests.post('https://api.line.me/v2/bot/message/reply',
-                  headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key("CHANNEL_ACCESS_TOKEN")}'},
+                  headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key(dotenv.find_dotenv(),"CHANNEL_ACCESS_TOKEN")}'},
                     json = { "replyToken":  body_json['events'][0]['replyToken'],
                             "messages": [{"type": "text", "text": "Sorry, Please specify the operation(ถอนฟัน, ผ่าฟันคุด, ผ่าตัดเหงือก, ผ่าตัดรากฟันเทียม) in the question."}]
                 })
@@ -225,7 +225,7 @@ def handle_message(body_json):
     })
 
     requests.post('https://api.line.me/v2/bot/message/reply',
-                  headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key("CHANNEL_ACCESS_TOKEN")}'},
+                  headers={'Content-Type': 'application/json', 'Authorization' : f'Bearer {dotenv.get_key(dotenv.find_dotenv(),"CHANNEL_ACCESS_TOKEN")}'},
                     json = { "replyToken":  body_json['events'][0]['replyToken'],
                             "messages": [{"type": "text", "text": output['answer']}]
                 })
@@ -263,4 +263,4 @@ def predict():
     return {'question': input_text, 'operation': Olabel[operation], 'question_type': Qlabel[prediction[0]], 'answer': output['answer'], 'score': output['score']}
 
 if __name__ == "__main__":
-    app.run(port=dotenv.get_key("MODEL_PORT"))
+    app.run(port=dotenv.get_key(dotenv.find_dotenv(),"MODEL_PORT"))
