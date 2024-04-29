4dc5a3b00286191f4010d4872bee259a62593105
KhChanon <chanon.619@hotmail.com>
2024-04-29 14:12:23 +0700

    Update API_URL and MODEL_URL in config.tsx and model server changes

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -1,7 +1,6 @@
 const config = {
-    "API_URL": `https://${process.env.REACT_APP_DNS_NAME}:8000/api`,
-    // "MODEL_URL": `https://${process.env.REACT_APP_DNS_NAME}:5000`,
-    "MODEL_URL": `https://Chanonk.pythonanywhere.com/`,
+    "API_URL": `${process.env.REACT_APP_API_URL}`,
+    "MODEL_URL": `${process.env.REACT_APP_MODEL_SERVER_NAME}`,
     "CLIENT_ID": "2003210748",
     "REDIRECT_URL": `https://${process.env.REACT_APP_DNS_NAME}:3000/`,
     "SCOPE": "profile%20openid%20email",
