f24128b626f12eae022e586ed1a3a7db0c7acdd7
KhChanon <chanon.619@hotmail.com>
2024-04-29 15:52:08 +0700

    Refactor server.js to remove HTTPS server setup and listen on the specified backend port

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 require('dotenv').config({ path: '../.env' })
 
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
@@ -31,17 +28,6 @@ app.use('/api/followup', require('./routes/followup.routes'));
 app.use('/api/dialogflow', require('./routes/dialogflow.routes'));
 
 
-const parentDirectory = path.join(__dirname, "..");
-
-// Read SSL certificate and key files
-const options = {
-    key: fs.readFileSync(path.join(parentDirectory, "certificates", "localhost-key.pem")),
-    cert: fs.readFileSync(path.join(parentDirectory, "certificates", "localhost.pem")),
-  };
-  
-// Create HTTPS server
-const server = https.createServer(options, app);
-
-server.listen(process.env.BACKEND_PORT, () => {
+app.listen(process.env.BACKEND_PORT, () => {
     console.log(`Server is running on port ${process.env.BACKEND_PORT}.`);
   });
