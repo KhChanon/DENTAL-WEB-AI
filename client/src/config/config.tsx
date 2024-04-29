ea7edb02f216b011aee016895fd08c5c000fbffa
KhChanon <chanon.619@hotmail.com>
2024-04-29 16:35:09 +0700

    Update REDIRECT_URL in config.tsx to use the new deployment URL

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -2,7 +2,7 @@ const config = {
     "API_URL": `${process.env.REACT_APP_API_URL}`,
     "MODEL_URL": `${process.env.REACT_APP_MODEL_SERVER_NAME}`,
     "CLIENT_ID": "2003210748",
-    "REDIRECT_URL": `https://${process.env.REACT_APP_DNS_NAME}:3000/`,
+    "REDIRECT_URL": `https://oralai.onrender.com/`,
     "SCOPE": "profile%20openid%20email",
 }
 
